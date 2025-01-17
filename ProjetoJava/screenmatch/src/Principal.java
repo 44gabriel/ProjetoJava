e6cd8c0caca0bc8773a359ba8fcaa4b5c9f18a59
Gabriel Diamante <98065603+44gabriel@users.noreply.github.com>
2025-01-17 14:39:50 -0300

    Add files via upload

---
+++ ProjetoJava/screenmatch/src/Principal.java
@@ -0,0 +1,18 @@
+import br.com.alura.screenmatch.modelo.Filme;
+
+public class Principal {
+    public static void main(String[] args) {
+        Filme meuFilme = new Filme();
+        meuFilme.setNome(" O Poderoso Chefão");
+        meuFilme.setAnoDelancamento(1970);
+        meuFilme.setDuracaoEmMinutos(180);
+
+
+        meuFilme.exibeFichaTecnica();
+        meuFilme.avalia(8);
+        meuFilme.avalia(5);
+        meuFilme.avalia(10);
+        System.out.println("Total de avaliações" + meuFilme.getTotalDeAvalaicoes());
+        System.out.println(meuFilme.pegaMedia());
+    }
+}
